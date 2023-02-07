4db91951c6fad88d8c1361044f5aa36f9a9398d7
koadegno <vinove9@hotmail.fr>
2023-02-07 17:16:26 +0100

    function made

--- test/downlaod_data.py
+++ test/downlaod_data.py
@@ -5,15 +5,8 @@ import pandas as pd
 from lsfb_dataset.utils.download.dataset_downloader import DatasetDownloader
 
 downloader = DatasetDownloader(
-    "./data/subset",
-    dataset="cont",
-    split='test',       # Only download the 'test' subset of the dataset
-    landmarks=[         # Choose landmarks. default is pose and hands
-        'pose',
-        'hands',
-        'face'
-    ],
-    include_raw=True,   # Also download raw landmarks (without interpolation and smoothing)
+    "./data/subset/isol",
     include_video=True,
+    split='test',
 )
 downloader.download()
