215c4b4f68d989dc68399fd229a4c7c8ac48f7cf
koadegno <vinove9@hotmail.fr>
2023-02-08 21:33:52 +0100

    update download data create function

--- test/downlaod_data.py
+++ test/downlaod_data.py
@@ -4,9 +4,29 @@ import pandas as pd
 
 from lsfb_dataset.utils.download.dataset_downloader import DatasetDownloader
 
-downloader = DatasetDownloader(
+def donwload_isol_video():
+    downloader = DatasetDownloader(
     "./data/subset/isol",
     include_video=True,
     split='test',
-)
-downloader.download()
+    )
+    downloader.download()
+
+def download_cont_video():
+    downloader = DatasetDownloader(
+    "./destination/folder",
+    dataset="cont",
+    split='test',       # Only download the 'test' subset of the dataset
+    landmarks=[         # Choose landmarks. default is pose and hands
+        'pose',
+        'hands',
+        'face'
+    ],
+    include_raw=True,   # Also download raw landmarks (without interpolation and smoothing)
+    include_video=True,
+    )
+    downloader.download()
+
+if __name__ == "__main__":
+    #download_cont_video()
+    donwload_isol_video()
